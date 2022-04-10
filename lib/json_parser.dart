b2376d4325f4ce2649e413fb20d15f9aa0efcda0
Eason Chen <easoncxz@gmail.com>
2022-04-11 00:34:08 +0300

    Initial commit: official dart "package-simple" template

---
+++ lib/json_parser.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library json_parser;
+
+export 'src/json_parser_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
